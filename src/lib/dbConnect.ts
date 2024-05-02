a1461ec8336b06fd24f63a25bb2e441c19c09c01
Farmaan16 <farmanmuzammil17gmail.com>
2024-05-02 19:53:40 +0530

    "Added db connection file"

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,30 @@
+import { log } from "console";
+import mongoose from "mongoose";
+
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {};
+
+
+ async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("already connected");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI!);
+        
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("connected to db");
+    } catch (error) {
+        console.log(error, "Could not connect to db");
+        process.exit(1);
+    }
+}
+
+export default dbConnect
